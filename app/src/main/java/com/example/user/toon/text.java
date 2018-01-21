975bdef46ee2b6d8b189b114fab86e67524e27c8
earnkito <kito.kamamiya@gmail.com>
2018-01-21 17:02:04 +0700

    toonn

---
+++ app/src/main/java/com/example/user/toon/text.java
@@ -0,0 +1,13 @@
+package com.example.user.toon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class text extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text);
+    }
+}
